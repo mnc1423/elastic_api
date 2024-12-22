c48976d5dcdd2b26678155dc17bdab672f29cdf0
mnc1423 <runegod123@naver.com>
2024-12-22 18:30:51 +0900

    added search Services

--- app/services/search_services.py
+++ app/services/search_services.py
@@ -2,10 +2,13 @@ from app.models.es_helpers import ESHelpers
 from app.models.models import es_settings
 
 
-async def elastic_wildcard_search(search_term: str):
-    query_ = {"query": {"wildcard": {"field": {"value": "*" + search_term + "*"}}}}
+async def elastic_wildcard_search(
+    search_term: str, search_field: str, search_index: str
+):
+
     async with ESHelpers(async_mode=True) as es:
-        resp = es.query_search(
-            query_=query_,
-            search_index=es_settings.es_index.my_data_index,
-        )
+        query_ = {
+            "query": {"wildcard": {search_field: {"value": "*" + search_term + "*"}}}
+        }
+        resp = await es.query_search(query_=query_, search_index=search_index)
+        return resp
