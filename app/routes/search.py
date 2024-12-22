ac33d4596ef5282071eb68de3acfe244bb649b07
mnc1423 <runegod123@naver.com>
2024-12-22 17:36:25 +0900

    changes to search

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,12 +1,16 @@
 import datetime
 from fastapi import APIRouter, status, HTTPException
 import app.services.search_services as searchServices
+from app.models.es_settings import ESHelpers
 
 search = APIRouter(prefix="/search", tags=["Search"])
 
 @search.get("/wildcard")
 async def wildcard_search(request):
-    pass
+    async with ESHelpers(async_mode=True) as es:
+        resp = await.query_search()
+
+    # resp = await  
 
 @search.get("/term")
 async def term_search(request):
