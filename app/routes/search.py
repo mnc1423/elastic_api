2ea84af5c1e9c16a1b434a7df2fa190c1f34fd0b
mnc1423 <runegod123@naver.com>
2024-12-22 18:30:32 +0900

    added Search functions

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,42 +1,51 @@
 import datetime
 from fastapi import APIRouter, status, HTTPException
+
 import app.services.search_services as searchServices
 from app.models.es_settings import ESHelpers
+from app.models.models import ElasticSeachRequest, ElasticSearchResponse
 
 search = APIRouter(prefix="/search", tags=["Search"])
 
-@search.get("/wildcard")
-async def wildcard_search(request):
+
+@search.get(
+    "/wildcard",
+    response_model=ElasticSearchResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def wildcard_search(request: ElasticSeachRequest):
+
+    resp = await searchServices.elastic_wildcard_search(
+        search_term=request["search_term"],
+        search_field=request["search_field"],
+        search_index=request["search_index"],
+    )
+    if resp:
+        return resp
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Failed to search "
+        )
+
+
+@search.get(
+    "/term",
+    response_model=ElasticSearchResponse,
+    status_code=status.HTTP_200_ok,
+)
+async def term_search(request: ElasticSeachRequest):
     async with ESHelpers(async_mode=True) as es:
         query_ = {
-            "query":{
-                "wildcard":{
-                    request['field']:{
-                        "value": "*" + request['search_term'] + "*"
-                    }
-                }
-            }
+            "query": {"term": {request["field"]: {"value": request["search_term"]}}}
         }
-        resp = await es.query_search(query_=query_, search_index=request['_index'])
+        resp = await es.query_search(query_=query_, search_index=request["_index"])
         return resp
 
 
-@search.get("/term")
-async def term_search(request):
-    async with ESHelpers(async_mode=True) as es:
-        query_ = {
-            "query":{
-                "term":{
-                    request['field']:{
-                        "value": request['search_term']
-                    }
-                }
-            }
-        }
-        resp = await es.query_search(query_=query_, search_index=request['_index'])
-        return resp
-
-
-@search.get("vector_search")
-async def vector_search(request):
+@search.get(
+    "vector_search",
+    response_model=ElasticSearchResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def vector_search(request: ElasticSeachRequest):
     pass
